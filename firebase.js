f4d05b16b66ed6b4e53582687ff1711d5a153e5b
akshar51 <aksharparekh401@gmail.com>
2025-07-05 14:18:07 +0530

    done

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
 import { initializeApp } from "firebase/app";
-
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
